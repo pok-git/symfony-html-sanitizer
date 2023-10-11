23150a7758d05bad11ddc84c704c4ec9730e1f45
Polychronis <PolychronisKampylis@gmail.com>
2023-10-11 16:16:58 +0300

    home page edited

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,29 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
+        $sample1 = 'Safe Content 1
+&lt;div&gt; Unsafe Content 1 &lt;/div&gt;
+&lt;!-- [if gte mso 9]&gt;&lt;xml&gt;
+ &lt;o:OfficeDocumentSettings&gt;&lt;o:RelyOnVML/&gt;&lt;o:AllowPNG/&gt;&lt;/o:OfficeDocumentSettings&gt;
+&lt;/xml&gt;&lt;![endif]--&gt;';
+
+        $sample2 = 'Safe Content 2
+&lt;div&gt; Unsafe Content 2 &lt;/div&gt;
+&lt;script&gt;// &lt;![CDATA[
+    (function(i,s,o,g,r,a,m){var ql=document.querySelectorAll(&#039;A[quiz],DIV[quiz]&#039;);
+// ]]&gt;&lt;/script&gt;';
+
+
+
+
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+            'sample1' => $sample1,
+            'sample2' => $sample2,
         ]);
     }
+
+
+
+
 }
